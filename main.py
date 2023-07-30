e20c0b5373f845437c8fc9372b0430783f9df066
Jordan8899 <86272781+Jordan8899@users.noreply.github.com>
2023-07-30 22:47:10 +0000

    Added files for the program and the future images and added comments to help with future programming

---
+++ main.py
@@ -0,0 +1,9 @@
+# Imports, pygame is a library allowing for a windowed game to be played
+# Functions is a file with the functions within this program making it easier to navigate
+import pygame
+import functions
+
+
+
+
+# Main Routine
